7dbd881886c4cf8c7144fa20523f7f17c6037a3d
Ivan Kraus <ivan.kraus@post.cz>
2023-12-13 11:00:12 +0100

    změna

--- lost-on-island/Services/LocationProvider.cs
+++ lost-on-island/Services/LocationProvider.cs
@@ -16,7 +16,8 @@ namespace lost_on_island.Services
             _locations.Add(new Location { Id = 2, Title = "Les", ImagePath = "Images/forest.png", Description = "Lesík s prasátky." });
             _locations.Add(new Location { Id = 3, Title = "Pláž", ImagePath = "Images/shipwreck.png", Description = "Písečná pláž s výhledem na loď." });
             _locations.Add(new Location { Id = 4, Title = "Hluboký les", ImagePath = "Images/forest.png", Description = "Lesík s prasátky." });
-
+            _locations.Add(new Location { Id = 3, Title = "Pláž", ImagePath = "Images/shipwreck.png", Description = "Písečná pláž s výhledem na loď." });
+            _locations.Add(new Location { Id = 4, Title = "Hluboký les", ImagePath = "Images/forest.png", Description = "Lesík s prasátky." });
 
 
             _connections.Add(new Connection { FromLocationId = 1, ToLocationId = 2, Description = "Les" });
